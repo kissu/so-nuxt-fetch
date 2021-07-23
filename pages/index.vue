06ae57c93745ca4e27e6c6923a816a2d73b2f1c8
Konstantin BIFERT <konstantin.bifert@gmail.com>
2021-07-23 20:14:17 +0200

    done

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{ opp }}
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ params, error, $http }) {
+    try {
+      const res = await $http.$get(
+        'https://jsonplaceholder.typicode.com/todos/1'
+      )
+      console.log('here we go', res)
+      return { opp: res }
+    } catch (err) {
+      error({ statusCode: 404, message: `User not found, ${err}` })
+    }
+  },
+}
+</script>
---
+++ pages/subjects/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>subjects index</div>
+</template>
